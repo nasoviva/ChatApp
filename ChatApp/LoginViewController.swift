93761842fab41db336b7fbd8d010cf365461d2d0
nasoviva <nasoviva@gmail.com>
2024-06-25 22:37:39 -0300

    add views

---
+++ ChatApp/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  ChatApp
+//
+//  Created by hverda on 25/06/2024.
+//
+
+import Foundation
--- ChatApp/WelcomeViewController.swift
+++ ChatApp/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  ChatApp
 //
 //  Created by hverda on 25/06/2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
